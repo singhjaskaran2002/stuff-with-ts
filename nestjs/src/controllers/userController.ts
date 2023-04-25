d1ca1b21d95c7e51016e5d5607d52396eb77dffe
Jaskaran Singh <singhjaskaran975@gmail.com>
2023-04-25 11:55:21 +0530

    refactored code

--- nestjs/src/controllers/userController.ts
+++ nestjs/src/controllers/userController.ts
@@ -2,20 +2,20 @@ import express from 'express';
 
 import { Controller } from '../decorators/controller';
 import { Route } from '../decorators/route';
+import { users } from '../fakeData/users';
 
 @Controller('/users')
 export class UserController {
 	@Route('get', '/')
 	static getAllUsers(_: express.Request, res: express.Response) {
-		res.send([
-			{ id: 1, name: 'John' },
-			{ id: 2, name: 'Jane' },
-		]);
+		res.json(users);
 	}
 
 	@Route('get', '/:id')
 	static getUserById(req: express.Request, res: express.Response) {
-		const userId = req.params.id;
-		res.send({ id: userId, name: 'John' });
+		const userId = +req.params.id;
+		const user = users.find((item) => item.id === userId);
+		if (!user) return res.status(404).json('NOT FOUND');
+		res.json(user);
 	}
 }
---
+++ nestjs/src/fakeData/users.ts
@@ -0,0 +1,9 @@
+import { IUser } from '../interfaces/user.js';
+
+export const users: IUser[] = [
+	{
+		email: 'jaskaran@yopmail.com',
+		name: 'Jaskaran Singh',
+		id: 1,
+	},
+];
